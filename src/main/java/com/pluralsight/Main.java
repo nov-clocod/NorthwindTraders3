f7f9840181d1c2c9b7cb7cfe229b69a398b0ec3b
Sthealth <AW@example.com>
2025-11-25 14:09:56 -0500

    add method to display all product information

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class Main {
@@ -44,6 +48,44 @@ public class Main {
             }
         } catch (Exception ex) {
             System.out.println("Please check your inputs!");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void displayAllProducts(String username, String password) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    username,
+                    password
+            );
+
+            String query = """
+                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                    FROM products
+                    """;
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet results = preparedStatement.executeQuery();
+
+            System.out.println("ID  " + " Name" + " ".repeat(37) + "Price   " + "Stock");
+            System.out.println("---- " + "-".repeat(40) + " -------" + " -----");
+
+            while (results.next()) {
+                int productID = results.getInt(1);
+                String productName = results.getString(2);
+                double productPrice = results.getDouble(3);
+                int productStock = results.getInt(4);
+
+
+                System.out.printf("%-4d %-40s %-7.2f %-5d\n",
+                        productID, productName, productPrice, productStock);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error occur!");
             System.out.println(ex.getMessage());
         }
     }
